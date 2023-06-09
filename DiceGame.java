fc3e2d8ea16502bad883d28ea72c222e493f98d8
s1300148 <s1300148@std4dc8>
2023-06-09 11:23:49 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,11 @@
+class DiceGame{
+    public static void main(String[] args){
+	int a=(int)(Math.random()*6)+1;
+	int b=(int)(Math.random()*6)+1;
+	int c=a+b;
+	System.out.println("Rolling dice...");
+	System.out.println("Die 1: "+a);
+	System.out.println("Die 2: "+b);
+	System.out.println("Total value: "+c);
+    }
+}
